f4df50d1b517368cebf277e56aa437d587eb4462
cristicadis <cristi.cadis@yahoo.com>
2024-01-20 23:49:41 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Navbar from './components/Navbar'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Alert from './components/Alert';
+import TextForm from './components/TextForm';
+import About from './components/About';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-     texteditor
+      <Router>
+      <Navbar  />
+      <Alert />
+        <div className="container my-3">
+
+          <Routes>
+            <Route path="/About" element={<About/>}>
+            </Route>
+
+            <Route path="/" element={<TextForm />}>   
+            </Route>
+          </Routes>
+
+          
+        </div>
+
+
+      </Router>
     </>
+     
+    
   )
 }
 
